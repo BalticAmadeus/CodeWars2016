0b3e678f2d0461527d9d4e6ebd46d7dbca29736f
Antanas Kompanas <a.kompanas@baltic-amadeus.lt>
2017-02-02 14:55:34 +0200

    Improved client example.

--- Game/Game.ClientHandler/Program.cs
+++ Game/Game.ClientHandler/Program.cs
@@ -9,6 +9,8 @@ namespace Game.ClientHandler
 {
     class Program
     {
+        static Random _rnd = new Random();
+
         static void Main(string[] args)
         {
             if (args.Length != 2)
@@ -36,25 +38,119 @@ namespace Game.ClientHandler
 
         static PlayerMove PerformTecmanMove(PlayerView view)
         {
-            // Just stay
+            // Find the nearest cookie
+            EnPoint direction;
+            ShortestPathToCookie(view.Map, view.TecmanPosition, out direction);
             return new PlayerMove
             {
-                Positions = new EnPoint[] { view.TecmanPosition }.ToList()
+                Positions = new EnPoint[] { direction ?? view.TecmanPosition }.ToList()
             };
+        }
+
+        static int ShortestPathToCookie(EnMapData map, EnPoint from, out EnPoint direction)
+        {
+            direction = null;
+            if (map.Rows[from.Row][from.Col] == '.')
+            {
+                return 0;
+            }
+            Tuple<int, EnPoint>[,] crumbs = new Tuple<int, EnPoint>[map.Height, map.Width];
+            for (int row = 0; row < map.Height; row++)
+            {
+                for (int col = 0; col < map.Width; col++)
+                {
+                    crumbs[row, col] = new Tuple<int, EnPoint>(1000000, null);
+                }
+            }
+            var front = new Queue<EnPoint>();
+            crumbs[from.Row, from.Col] = new Tuple<int, EnPoint>(0, null);
+            front.Enqueue(from);
+            while (front.Count > 0)
+            {
+                EnPoint x = front.Dequeue();
+                for (int dr = -1; dr <= 1; dr++)
+                {
+                    for (int dc = -1; dc <= 1; dc++)
+                    {
+                        if (dr * dc != 0 || dr + dc == 0)
+                            continue;
+                        var y = new EnPoint { Row = x.Row + dr, Col = x.Col + dc };
+                        if (y.Row < 0)
+                            y.Row = map.Height - 1;
+                        else if (y.Row >= map.Height)
+                            y.Row = 0;
+                        if (y.Col < 0)
+                            y.Col = map.Width - 1;
+                        else if (y.Col >= map.Width)
+                            y.Col = 0;
+                        if (map.Rows[y.Row][y.Col] == '#')
+                            continue;
+                        Tuple<int, EnPoint> t = crumbs[x.Row, x.Col];
+                        int v = t.Item1 + 1;
+                        if (crumbs[y.Row, y.Col].Item1 <= v)
+                            continue;
+                        crumbs[y.Row, y.Col] = new Tuple<int, EnPoint>(v, t.Item2 ?? y);
+                        if (map.Rows[y.Row][y.Col] == '.')
+                        {
+                            direction = t.Item2 ?? y;
+                            return v;
+                        }
+                        front.Enqueue(y);
+                    }
+                }
+            }
+            return 2000000;
         }
 
         static PlayerMove PerformGhostMove(PlayerView view)
         {
             var move = new PlayerMove
             {
-                Positions = new List<EnPoint>(view.GhostPositions)
+                Positions = new List<EnPoint>()
             };
-            // Move all ghosts to the right. It's short-sited but probably valid move.
-            for (int i = 0; i < move.Positions.Count; i++)
+            // Move all ghosts randomly
+            for (int i = 0; i < view.GhostPositions.Count; i++)
             {
-                move.Positions[i].Col++;
+                var options = new List<EnPoint>();
+                addGhostOption(options, view, i, 0, 1);
+                addGhostOption(options, view, i, 0, -1);
+                addGhostOption(options, view, i, 1, 0);
+                addGhostOption(options, view, i, -1, 0);
+                switch (options.Count)
+                {
+                    case 0:
+                        move.Positions.Add(view.PreviousGhostPositions[i]);
+                        break;
+                    case 1:
+                        move.Positions.Add(options[0]);
+                        break;
+                    default:
+                        move.Positions.Add(options[_rnd.Next(options.Count)]);
+                        break;
+                }
             }
             return move;
+        }
+
+        static void addGhostOption(List<EnPoint> options, PlayerView view, int ghost, int drow, int dcol)
+        {
+            var target = new EnPoint { Row = view.GhostPositions[ghost].Row + drow, Col = view.GhostPositions[ghost].Col + dcol };
+
+            if (target.Row < 0)
+                target.Row = view.Map.Height - 1;
+            else if (target.Row >= view.Map.Height)
+                target.Row = 0;
+
+            if (target.Col < 0)
+                target.Col = view.Map.Width - 1;
+            else if (target.Col >= view.Map.Width)
+                target.Col = 0;
+
+            if (view.Map.Rows[target.Row][target.Col] != '#'
+                && (target.Row != view.PreviousGhostPositions[ghost].Row || target.Col != view.PreviousGhostPositions[ghost].Col))
+            {
+                options.Add(target);
+            }
         }
     }
 
